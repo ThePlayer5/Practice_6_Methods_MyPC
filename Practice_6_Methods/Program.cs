86b3033e3f07ccb16a446fa49b11215e0b09b4e5
GOOGLE228 <75929954+GOOGLE228@users.noreply.github.com>
2025-02-16 16:18:35 +0300

    Добавьте файлы проекта.

---
+++ Practice_6_Methods/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Practice_6_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            Console.WriteLine("Введите числа: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите номер операции: ");
+            Console.WriteLine($"Результат: {Calcul(a, b)}");
+
+            // Задание 2
+            //Console.WriteLine("Введите числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Минимальное: {Min(a, b)}");
+            //Console.WriteLine($"Максимальное: {Max(a, b)}");
+
+            // Задание 3
+            //bool per = true;
+            //Console.WriteLine("Введите слово: ");
+            //IsPalindrome(per);
+
+            // Задание 4
+            //Console.WriteLine("Введите число: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write($"Факториал {n} = {Factorial(n)}");
+
+            // Задание 5
+            //Console.WriteLine("Введите числа: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //double result = Average(a, b, c);
+            //Console.WriteLine($"Среднее арифмитическое: {Math.Round(result, 1)}");
+
+            // Задание 6
+            //Console.Write("Введите строку: ");
+            //string text = Console.ReadLine();
+            //Console.Write("Введите символ: ");
+            //char c = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(FindChar(text, c));
+
+            // Задание 7
+            //Console.Write("Введите длину пароля: ");
+            //int length = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(GeneratePassword(length));
+
+            // Задание 8
+            //Console.WriteLine("Введите градусы: ");
+            //Console.Write("Градусы по Цельсию: ");
+            //double c = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Градусы по Фаренгейту: ");
+            //double f = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine($"Из Цельсия в Фаренгейты: {Math.Round(CelsiusToFahrenheit(c), 1)}");
+            //Console.WriteLine($"Из Фаренгейты в Цельсия: {Math.Round(FahrenheitToCelsius(f), 1)}");
+
+            // Задание 9
+            //string sentence = Console.ReadLine();
+            //ReverseWords(sentence);
+
+            // Задание 10
+            //Console.Write("Введите число: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Таблица умножения числа {n}:\n{MultiplicationTable(n)}");
+
+            Console.ReadKey();
+        }
+        // Задание 1
+        public static int Calcul(int a, int b)
+        {
+            int c = Convert.ToInt32(Console.ReadLine());
+            switch (c)
+            {
+                case 1:
+                    return a + b;
+                    break;
+                case 2:
+                    return a - b;
+                    break;
+                case 3:
+                    return a * b;
+                    break;
+                case 4:
+                    return a / b;
+                    break;
+                default:
+                    Console.WriteLine("Такой операции нету");
+                    return c;
+            }
+        }
+        // Задание 2
+        public static int Min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+        public static int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+        // Задание 3
+        public static bool IsPalindrome(bool per)
+        {
+            string str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == str[str.Length - 1 - i])
+                {
+                    per = true;
+                }
+                else
+                {
+                    per = false;
+                }
+            }
+            if (per == true) Console.WriteLine("Это полиндром");
+            else Console.WriteLine("Это НЕ полиндром");
+            return per;
+        }
+        // Задание 4
+        public static int Factorial(int n)
+        {
+            int c = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                c = c * i;
+            }
+            n = c;
+            return n;
+        }
+        // Задание 5
+        public static double Average(int a, int b, int c)
+        // Тут можно после static добавить double (вместо int) и метод Average будет возвращать уже дробное значение
+        {
+            double d = 3.0;
+            return (a + b + c) / d;
+        }
+        // Задание 6
+        public static int FindChar(string text, char c)
+        {
+            if (text.Contains(c)) return text.IndexOf(c);
+            else return -1;
+        }
+        // Задание 7
+        public static string GeneratePassword(int length)
+        {
+            Random random = new Random();
+            List<int> list = new List<int>();
+            string str = null;
+            for (int i = 0; i < length; i++)
+            {
+                list.Add(random.Next(0, 10));
+                str += list[i].ToString();
+            }
+            return str;
+        }
+        // Задание 8
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return c * 9 / 5 + 32;
+        }
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) * 5 / 9;
+        }
+        // Задание 9
+        public static string ReverseWords(string sentence)
+        {
+            string[] mass = sentence.Split(' ', ',');
+            for (int i = mass.Length - 1; i >= 0; i--)
+            {
+                Console.Write(mass[i] + " ");
+            }
+            return sentence;
+        }
+
+        // Задание 10
+        public static string MultiplicationTable(int n)
+        {
+            string c = null;
+            for (int i = 1; i <= 10; i++)
+            {
+                c += $"{n} x {i} = {n * i}\n";
+            }
+            return c;
+        }
+    }
+}
---
+++ Theory/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theory
+{
+    internal class Program
+    {
+        // Пробую задание 9 сделать
+        static void Main(string[] args)
+        {
+            // Попробуй сделать задание 9 из ПР 6 с помощью Replace, а то у меня не выходит, пока что
+
+            string sentence = Console.ReadLine();
+            string[] mass = sentence.Split(' '); // { "hello", "world" }
+            List<string> list = new List<string>();
+            //for (int i = 0; i < mass.Length - 1; i++)
+            //{
+            //    //mass[i] = mass[i].Replace(mass[i], mass[i + 1]);
+            //    //mass[i].Replace(mass[i + 1], mass[i]);
+            //    mass
+            //}
+            //Console.WriteLine(sentence.Replace("hello", "world"));
+            for (int i = 0; i < mass.Length - 1; i++)
+            {
+                if (list.Contains(mass[i]))
+                {
+                    sentence.Remove(sentence.IndexOf(mass[i]));
+                }
+                list.Add(sentence.Replace(mass[i], mass[i + 1]));
+                Console.Write(" " + mass[i]);
+                //else
+                //{
+                //    sentence += mass[i];
+                //    Console.Write(sentence);
+                //}
+                //Console.WriteLine(i);
+                //Console.WriteLine(sentence.Replace(mass[i + 1], mass[i]));
+            }
+            
+            //string word1 = null;
+            //string word2 = null;
+            //if (sentence.Contains(' '))
+            //{
+            //    for (int i = 0; i < sentence.Length / 2; i++)
+            //    {
+            //        word1 += sentence[i];
+            //    }
+            //    for (int i = sentence.Length / 2; i < sentence.Length; i++)
+            //    {
+            //        word2 += sentence[i];
+            //    }
+            //    sentence = word2 + " " + word1;
+            //    Console.WriteLine(sentence);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(sentence);
+            //}
+
+
+            //for (int i = 0; i < sentence.Length; i++)
+            //{
+            //    if (sentence[i] == ' ')
+            //    {
+            //        break;
+            //    }
+            //    else word1 += sentence[i];
+            //}
+            //for (int i = 0; i < sentence.Length; i++)
+            //{
+            //    if (sentence[i] == ' ')
+            //    {
+            //        break;
+            //    }
+            //    else word2 = sentence.Remove(i);
+            //}
+            //Console.WriteLine(word2 + " " + word1);
+
+            //for (int i = 0; i < sentence.Length; i++)
+            //{
+            //    if (sentence[i] == ' ')
+            //    {
+            //        break;
+            //    }
+            //    word1 += sentence[i];
+            //}
+            //for (int i = 0;i < sentence.Length; i++)
+            //{
+            //    if (sentence[i] == ' ')
+            //    {
+            //        word2 += sentence[i + 1];
+            //    }
+            //    else continue;
+            //}
+            //Console.WriteLine(word1);
+            //Console.WriteLine(word2);
+            //Console.WriteLine(sentence.Replace(sentence[1], sentence[0]));
+            Console.ReadKey();
+        }
+        //public static int Add(int x, int y)
+        //{
+        //    return x + y; // 8
+        //}
+        //public static void MessageMethod()
+        //{
+        //    Console.WriteLine("My message");
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int result = Add(5, 9);
+        //    Console.WriteLine(result);
+        //    Console.WriteLine(Add(1, 3));
+        //    MessageMethod();
+        //    string per = Console.ReadLine();
+        //    MessageMethod2(per);
+        //    int x = 3;
+        //    int y = 5;
+        //    Console.WriteLine(Add(x, y)); // 8
+        //    Console.WriteLine(Add(x, y) + 6); // 14
+        //    MessageMethod(); // "My message"
+
+        //    Console.ReadKey();
+
+
+        //    Console.ReadKey();
+        //}
+        //public static int Add(int a, int b)
+        //{
+        //    a = a * a;
+        //    b = b * b;
+        //    //int y = a + b;
+        //    //return y;
+        //    // ИЛИ
+        //    return a + b;
+        //}
+        //public static void MessageMethod()
+        //{
+        //    Console.WriteLine("Bubuu aguguguggu");
+        //}
+        //public static void MessageMethod2(string message)
+        //{
+        //    Console.WriteLine($"{message}");
+        //}
+
+
+    }
+}
